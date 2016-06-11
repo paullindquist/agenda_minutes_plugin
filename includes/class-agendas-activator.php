58d6ef2e47062f92ced0fc492655e037d392a012
Paul Lindquist <paul.lindquist@gmail.com>
2016-06-11 18:52:46 -0500

    added mustache and created db

--- includes/class-agendas-activator.php
+++ includes/class-agendas-activator.php
@@ -3,7 +3,7 @@
 /**
  * Fired during plugin activation
  *
- * @link       http://example.com
+ * @link       http://frontsideapps.com
  * @since      1.0.0
  *
  * @package    Agendas
@@ -30,6 +30,18 @@ class Agendas_Activator {
 	 * @since    1.0.0
 	 */
 	public static function activate() {
+		global $wpdb;
+		$charset_collate = $wpdb->get_charset_collate();
+		$table_name = $wpdb->prefix . 'agendas';
+		$sql = "CREATE TABLE $table_name (
+			id mediumint(9) NOT NULL AUTO_INCREMENT,
+			date datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+			agenda smallint(5) NOT NULL,
+			minutes smallint(5) NOT NULL,
+			UNIQUE KEY id (id)
+		) $charset_collate;";
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		dbDelta( $sql );
 
 	}
 
